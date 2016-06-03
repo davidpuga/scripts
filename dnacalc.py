adeb5c4ad3549b73081e2228386bc3a04b05bc88
David Puga <david.puga@embl.de>
2016-06-03 11:12:36 +0200

    added dnacalc

---
+++ dnacalc.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+# python is on different parts in different computers. env is used to open it no matter where it is
+
+DNASeq = 'ATGAAC'
+# use variables with descriptive names. This variable is a string.
+
+print ( 'Sequence ' + DNASeq )
+
